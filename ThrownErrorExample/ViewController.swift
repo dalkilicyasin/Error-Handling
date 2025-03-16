fa99366aeb4911fcdf9944e65ce881f9087b5ab9
Yasin Dalkılıç <ysndlklc1989@icloud.com>
2025-03-16 22:25:55 +0300

    Initial Commit

---
+++ ThrownErrorExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThrownErrorExample
+//
+//  Created by Yasin Dalkılıç on 16.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ThrownErrorExampleTests/ThrownErrorExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThrownErrorExampleTests.swift
+//  ThrownErrorExampleTests
+//
+//  Created by Yasin Dalkılıç on 16.03.2025.
+//
+
+import Testing
+@testable import ThrownErrorExample
+
+struct ThrownErrorExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
